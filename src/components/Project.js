72df2d00e3adbd4d5bbe9ddc4b6317ede91295ec
Anas Qazza <anasqazza@gmail.com>
2023-06-15 19:35:41 -0700

    Added one more fade

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { Fade } from "react-reveal";
 
 const Project = ({ item, description }) => {
   return (
-    <div key={item.id} className="flex flex-col items-center text-center">
-      <div className="mb-8">
-        <a href={item.link}>
-          <img className="rounded-2xl" src={item.image} alt="" />
-        </a>
+    <Fade>
+      <div key={item.id} className="flex flex-col items-center text-center">
+        <div className="mb-8">
+          <a href={item.link}>
+            <img className="rounded-2xl" src={item.image} alt="" />
+          </a>
+        </div>
+        <p className="capitalize text-accent text-sm mb-3">{item.category}</p>
+        <h3 className="text-2xl font-semibold capitalize mb-3">
+          <a href={item.link}>{item.name}</a>
+        </h3>
+        <p className="text-base max-w-md">{description}</p>
       </div>
-      <p className="capitalize text-accent text-sm mb-3">{item.category}</p>
-      <h3 className="text-2xl font-semibold capitalize mb-3">
-        <a href={item.link}>{item.name}</a>
-      </h3>
-      <p className="text-base max-w-md">{description}</p>
-    </div>
+    </Fade>
   );
 };
 
